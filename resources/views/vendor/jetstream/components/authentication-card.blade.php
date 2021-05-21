9591c5d0a50970ddd7b39ab47dce91ba300a5008
Monarul <monarul007@gmail.com>
2021-05-21 15:26:40 +0600

    database seeding changed

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -6,7 +6,7 @@
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
-    @if ( Route::current()->uri == 'login')
+    @if ( Route::current()->uri == 'login' || Route::current()->uri == '/')
     <br>
     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="/register" style="text-align: center;padding: 5px 10px;border-radius: 4px;background: white;">Don't have an account? Create here</a>
     @endif
